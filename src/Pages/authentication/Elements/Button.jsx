a4557e0b7db0e66f492ed14f2a6aee2593dabc08
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 18:14:41 +0300

    (style): add shadow effect to button component
    - Applied `shadow-2xl` for enhanced visual depth
    - Improved button appearance for better emphasis in UI

--- src/Pages/authentication/Elements/Button.jsx
+++ src/Pages/authentication/Elements/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ButtonText}) {
     return (
       <>
-        <button className="w-full bg-[#111827] rounded-full p-[14px] text-white text-[14px] font-medium">{ButtonText}</button>
+        <button className="w-full shadow-2xl bg-[#111827] rounded-full p-[14px] text-white text-[14px] font-medium">{ButtonText}</button>
       </>
     );
 }
