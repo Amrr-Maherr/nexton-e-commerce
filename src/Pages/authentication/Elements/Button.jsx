388dc55a032b7154e8ddd739d15eb20128a2c355
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 18:21:13 +0300

    (feat): add framer-motion animation to Button component**
    * Integrated `framer-motion` for the button element
    * Applied initial scale and opacity with smooth hover/tap effects
    * Enhanced user experience with modern motion design

--- src/Pages/authentication/Elements/Button.jsx
+++ src/Pages/authentication/Elements/Button.jsx
@@ -1,7 +1,17 @@
-export default function Button({ButtonText}) {
-    return (
-      <>
-        <button className="w-full shadow-2xl bg-[#111827] rounded-full p-[14px] text-white text-[14px] font-medium">{ButtonText}</button>
-      </>
-    );
+"use client";
+import { motion } from "motion/react";
+
+export default function Button({ ButtonText }) {
+  return (
+    <motion.button
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.4, ease: "easeOut" }}
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
+      className="w-full shadow-2xl bg-[#111827] rounded-full p-[14px] text-white text-[14px] font-medium cursor-pointer"
+    >
+      {ButtonText}
+    </motion.button>
+  );
 }
