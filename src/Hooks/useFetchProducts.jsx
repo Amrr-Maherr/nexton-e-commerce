c3beca0e2ee6c75926528082f491d72605f3fcef
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 21:59:33 +0300

    (feat): create useFetchProducts custom hook
    - Added a custom React hook `useFetchProducts` to fetch product data from API
    - Handles loading state, data storage, and error management
    - Can be reused across components to retrieve products from the endpoint

---
+++ src/Hooks/useFetchProducts.jsx
@@ -0,0 +1,30 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useFetchProducts = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await fetch(
+          "https://ecommerce.routemisr.com/api/v1/products"
+        );
+        const result = await res.json();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useFetchProducts;
