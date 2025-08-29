631b1750b56ece88db77ad61059dc2416381e4ab
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 17:48:15 +0300

    update

--- src/Pages/authentication/Elements/LineAcross.jsx
+++ src/Pages/authentication/Elements/LineAcross.jsx
@@ -1,8 +1,8 @@
 export default function LineAcross() {
   return (
     <div className="flex items-center w-full my-4">
-      <div className="flex-grow border-t border-gray-300"></div>
-      <span className="mx-4 text-[#F3F4F6] text-sm">OR</span>
+      <div className="flex-grow border-t border-[#F3F4F6]"></div>
+      <span className="mx-4 text-black text-sm">OR</span>
       <div className="flex-grow border-t border-gray-300"></div>
     </div>
   );
