c5fb84e169275d272c7d8c1a3a9b55350e5fa480
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 15:32:19 +0300

    (feat): replace Geist fonts with Roboto fonts
    - Updated Google font imports to use Roboto and Roboto Mono.
    - Added corresponding CSS variables --font-roboto and --font-roboto-mono.
    - Prepared RootLayout to apply the new fonts across the app.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
+  variable: "--font-roboto",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
