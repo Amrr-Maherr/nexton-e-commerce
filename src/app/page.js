b499d282627e855a28038dd6979eb32571af5c4b
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 21:16:36 +0300

    feat: add login navigation link with user icon
    - Added a `Link` component pointing to `/Login` page
    - Displayed a `User` icon inside the link with a `cursor-pointer` class for better UX

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,8 @@
-import Login from "@/app/Login/page";
-import Register from "@/app/Register/page";
-import Image from "next/image";
 import HomePage from "./Home/page";
 
 export default function Home() {
   return (
     <>
-      {/* <Login /> */}
       <HomePage/>
     </>
   );
--- src/components/Header/Elements/UserActions.jsx
+++ src/components/Header/Elements/UserActions.jsx
@@ -1,11 +1,14 @@
 import { User } from "lucide-react";
 import { ShoppingCart } from "lucide-react";
+import Link from "next/link";
 
 export default function UserActions() {
   return (
     <>
       <div className="flex items-center justify-center gap-[22px]">
-        <User className="cursor-pointer" />
+        <Link href="/Login">
+          <User className="cursor-pointer" />
+        </Link>
         <ShoppingCart className="cursor-pointer" />
       </div>
     </>
