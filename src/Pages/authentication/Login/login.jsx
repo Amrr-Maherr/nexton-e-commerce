835d62b74ccaec8c0fffd6cc0ec90f5d47fce93e
Amrr-Maherr <amrr.maherr242gmail.com>
2025-08-29 17:46:48 +0300

    update

--- src/Pages/authentication/Login/login.jsx
+++ src/Pages/authentication/Login/login.jsx
@@ -8,7 +8,7 @@ import Title from "../Elements/Title";
 export default function Login() {
     return (
       <>
-        <section className="pt-[40px] pb-[72px] max-w-[440px] mx-auto gap-[24px] flex items-center justify-center flex-col">
+        <section className="pt-[40px] pb-[72px] max-w-[440px] mx-auto gap-[24px] flex items-center justify-center flex-col px-[24px] md:px-0">
           <Title TitleText="Login" />
           <EmailInput />
           <PasswordInput />
