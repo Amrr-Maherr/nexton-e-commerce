84aa8892699649d6252af19e17f1b38fa3b8a1d7
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-01 10:56:48 +0300

    (feat): Use Next.js Image component for brand cards
    - Replace standard <img> with Next.js <Image> component
    - Set width, height, and quality for optimized loading
    - Ensure proper rendering and responsiveness of brand images

--- src/app/Home/Elements/BrandsSection/BrandCard.jsx
+++ src/app/Home/Elements/BrandsSection/BrandCard.jsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 export default function BrandCard({ brand }) {
     return <>
     <div>
-        <img src={brand.image} alt="" />
+        <Image width={1000} height={1000} quality={100} src={brand.image} alt="" />
     </div>
     </>;
 }
