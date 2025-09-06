8c8f627dc959dc385a23ebd815df2269292902c2
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-06 21:25:03 +0300

    update

---
+++ src/Redux/ProductsSlice.jsx
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+export const ProductsSlice = createSlice({
+    name: "All_Products",
+    initialState: [],
+    reducers: {
+        
+    }
+})
+export default ProductsSlice.reducer;
