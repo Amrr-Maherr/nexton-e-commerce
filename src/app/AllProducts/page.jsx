d6612906db660d9a99d61a07b3f5f3527f403b2e
Amrr-Maherr <amrr.maherr242gmail.com>
2025-09-12 06:59:57 +0300

    update

--- src/app/AllProducts/page.jsx
+++ src/app/AllProducts/page.jsx
@@ -1,27 +1,27 @@
 "use client";
-import { useState } from "react";
-import ProductCard from "../Home/Elements/ProductsSection/ProductCard";
-import FiltersUI from "./Elements/FiltersUI";
-import Loader from "@/components/Loader/Loader";
+// import { useState } from "react";
+// import ProductCard from "../Home/Elements/ProductsSection/ProductCard";
+// import FiltersUI from "./Elements/FiltersUI";
+// import Loader from "@/components/Loader/Loader";
 
 export default function AllProducts() {
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [filteredProducts, setFilteredProducts] = useState([]);
+  // const [loading, setLoading] = useState(false);
 
   return (
     <>
       <section>
         <div className="grid grid-cols-1 md:grid-cols-12 gap-5 py-5 md:py-22 container mx-auto">
           {/* Filters */}
-          <div className="col-span-12 md:col-span-3">
+          {/* <div className="col-span-12 md:col-span-3">
             <FiltersUI
               setFilteredProducts={setFilteredProducts}
               setLoading={setLoading}
             />
-          </div>
+          </div> */}
 
           {/* Products Grid */}
-          <div className="col-span-12 md:col-span-9 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-5">
+          {/* <div className="col-span-12 md:col-span-9 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-5">
             {loading ? (
               <Loader />
             ) : filteredProducts.length > 0 ? (
@@ -31,7 +31,7 @@ export default function AllProducts() {
             ) : (
               <p>No products found</p>
             )}
-          </div>
+          </div> */}
         </div>
       </section>
     </>
